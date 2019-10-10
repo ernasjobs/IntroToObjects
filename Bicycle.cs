1ab41fa3464630fe1df7a5a1208852f5767e2f16
Ernas Pikuli <ernasjobs@gmail.com>
2019-10-10 11:45:36 +0100

    Create Bicycle Class and add its attributes

---
+++ Bicycle.cs
@@ -0,0 +1,7 @@
+public class Bicycle{
+    private static double currentSpeed;
+    private static int currentGear;
+    private static bool pushedBell;
+    private static double steeringAngle;
+    
+}
