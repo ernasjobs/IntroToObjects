f0adf535a3c945c6b487b22b4e0808ee8ee421cc
Ernas Pikuli <ernasjobs@gmail.com>
2019-10-10 11:33:54 +0100

    Create C# Console App

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntroToObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
