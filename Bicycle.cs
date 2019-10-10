3befd4d22aec485f125c07e7e4bba457a341ac47
Ernas Pikuli <ernasjobs@gmail.com>
2019-10-10 12:00:03 +0100

    Add methods of the class

--- Bicycle.cs
+++ Bicycle.cs
@@ -1,7 +1,32 @@
+using System;
 public class Bicycle{
     private static double currentSpeed;
     private static int currentGear;
     private static bool pushedBell;
     private static double steeringAngle;
-    
+
+    public void Accelerate(){
+        currentSpeed+=10;
+    }
+    public void Decelerate(){
+        currentSpeed-=10;
+    }
+    public void ChangeGear(){
+        currentGear=0;
+    }
+    public void PushBell(){
+        pushedBell=false;
+    }
+    public void SetSteeringAngle(){
+        steeringAngle=25.5;
+    }
+
+    public void GetStates(){
+        Console.WriteLine("Current Speed is: "+currentSpeed);
+        Console.WriteLine("Current Gear is: "+currentGear);
+        Console.WriteLine("Has bell been pushed?"+pushedBell);
+        Console.WriteLine("Current steering angle is: "+steeringAngle);
+        Console.ReadKey(true);
+    }
+
 }
